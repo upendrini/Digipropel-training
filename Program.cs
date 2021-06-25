1770e5404c8357d7f245479c73c7b27465021216
upendrini <86408918+upendrini@users.noreply.github.com>
2021-06-25 15:06:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double num= 45.34;
+            Console.WriteLine(num);
+        }
+    }
+}
