29fcabc5cfc6dd6066a28f0ebbefac7d4099a51a
upendrini <86408918+upendrini@users.noreply.github.com>
2021-06-26 19:16:52 +0530

    update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
-namespace t3
+namespace _1q
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            double num= 45.34;
-            Console.WriteLine(num);
+            int a=1,b=2,c=3;
+            Console.WriteLine("enter a,b,c values");
+            Console.WriteLine("enter the value of a:",a);
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of b:",b);
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of c:",c);
+            c=Convert.ToInt32(Console.ReadLine());
+            int d=a*b*c;
+            Console.WriteLine("multiplication of three numbers:" +d);
+            Console.ReadLine();
         }
     }
 }
