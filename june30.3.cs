c042434137f36b27568b4fa2c1a047e1031eb8e8
upendrini <86408918+upendrini@users.noreply.github.com>
2021-07-07 15:45:01 +0530

    Add files via upload

---
+++ june30.3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace aug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+            Console.Write("\n\n");
+            Console.WriteLine("Display the pattern like 'B' with an asterisk:");
+            Console.Write("---------------------------------------------");
+            Console.Write("\n\n");
+            
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (column == 1 || ((row == 0 || row == 3 || row == 6) && (column < 5 && column > 1)) || (column == 5 && (row != 0 && row != 3 && row != 6)))
+                    Console.Write("*");
+                    else
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        
+    }
+}
+
+        
+    
+
