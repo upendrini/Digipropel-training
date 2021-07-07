9b11bc6e1b3aa5d06486715d0ec150229393ac3f
upendrini <86408918+upendrini@users.noreply.github.com>
2021-07-07 16:09:41 +0530

    Add files via upload

---
+++ june30.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace june27
+{
+    class Program
+    {
+        static void main()
+        {}
+        
+            enum days { Sun, Mon, Tue, Wed, Thur, Fri, Sat }
+        
+        
+        public void Enumprogram()
+        {
+            int Weekdaystart = (int)days.Mon;
+            int Weekdayend = (int)days.Sat;
+
+            Console.WriteLine("Mon:{0}", Weekdaystart);
+            Console.WriteLine("Sat:{0}", Weekdayend);
+
+        }
+        
+    }
+}
