c9e816693537a43a9621d151ff9f553911c9cfe5
upendrini <86408918+upendrini@users.noreply.github.com>
2021-07-09 15:30:35 +0530

    Add files via upload

---
+++ july05.8cs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace july
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number=new Random ();
+            int Random = number.Next(100);
+            Console.WriteLine(number);
+            Console.ReadKey();
+
+        }
+    }
+}
+         /*random number=5*/ 
